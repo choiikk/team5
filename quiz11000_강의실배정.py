f9012f371bcb19cc815c72449700380f6c7e9e44
choiikk <gurum6789@gmail.com>
2023-04-03 13:42:53 +0900

    그리디 과제

--- quiz11000_강의실배정.py
+++ quiz11000_강의실배정.py
@@ -1,1 +1,37 @@
-#끝나는시간 (T)기준으로 오름차 정렬. 클래스연결되서추가되었다는걸 알수잇게 
+#11000 강의실배정(실패) 왜 틀렸는지 모르겠다 ㅜㅜ 
+from queue import PriorityQueue
+import sys
+input = sys.stdin.readline 
+N = int(input())
+lectures=list()
+
+for i in range(N):
+    S, T = map(int, input().split(' '))
+    lectures.append((S,T))
+lectures.sort()# (강의) 시작 시간 기준으로 오름차순 정렬
+
+rooms = PriorityQueue() # 강의실
+rooms.put(lectures[0][1]) # 강의실에는 종료시간만 저장해 놨다가, 다음 배정할 강의의 시작시간과 비교하는데 쓴다
+
+
+for i in range(1,N):
+    nextLecStart = lectures[i][0]
+    nextLecEnd = lectures[i][1]
+
+
+
+    # if rooms[0] <= nextLecStart:#이어서 수업 가능 (새로운 강의실 필요 없음)
+    #     rooms.get() #이어서 수업할거니까 현재 종료 시간을 삭제 
+    # rooms.put(nextLecEnd) #이어서 수업한다면 종료시간을 업데이트 한 것이고, 이어서 수업할 수 없다면 강의실을 추가한 것이다
+        #우선순위큐는 인덱스 쓸 수 없다.......
+    
+    
+    if rooms.get() <= nextLecStart:#이어서 수업 가능 (새로운 강의실 필요 없음)
+        rooms.put(nextLecEnd) #이어서 수업한다면 종료시간을 업데이트
+    else:
+        rooms.put(lectures[0][1])
+        rooms.put(nextLecEnd) #이어서 수업할 수 없다면 강의실 추가 
+    
+print(rooms.qsize())
+        
+
---
+++ quiz1946_신입사원.py
@@ -0,0 +1,25 @@
+#1946 신입사원(실패 (답은 나오는거 같은데 시간초과, 어떻게 greedy 알고리즘으로 푸는지 모르겠다))
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+testCases=list() #테스트 케이스들의 리스트
+for i in range(T):
+    N = int(input())
+    tempCase=list()#각 테스트 케이스
+    for j in range(N):
+        score1, score2 = map(int, input().split(' '))#서류점수, 면접점수
+        tempCase.append((score1,score2))#튜플로 저장
+    testCases.append(tempCase) #각 테스트 케이스를 테스트 케이스들의 리스트에 저장
+
+#불합격 조건: 본인(i)이 속한 테스트 케이스에서, 서류와 면접 모두 순위가 높은(숫자가 작은) 사람(비교상대:j)이 있으면 나는 불합격
+for tempCase in testCases:
+    count=0 #각 테스트케이스 당 합격자 수
+    for i in tempCase:
+        ok = True #default는 합격
+        for j in tempCase:
+            if i[0]> j[0] and i[1]>j[1]: #불합격조건
+                ok = False
+        if ok:
+            count+=1
+    print(count)#결과 출력
